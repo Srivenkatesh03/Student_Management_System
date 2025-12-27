82794380bd562fd41e74cce21049f804f892b860
Srivenkatesh R <117419799+Srivenkatesh03@users.noreply.github.com>
2025-12-28 00:25:47 +0530

    Refactor code structure for improved readability and maintainability

--- core/signals.py
+++ core/signals.py
@@ -8,3 +8,26 @@ def delete_student_image(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+
+import os
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Student
+
+
+@receiver(pre_save, sender=Student)
+def delete_old_image_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old_instance = Student.objects.get(pk=instance.pk)
+    except Student.DoesNotExist:
+        return
+
+    old_image = old_instance.image
+    new_image = instance.image
+
+    if old_image and old_image != new_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
